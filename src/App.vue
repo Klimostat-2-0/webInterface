5f4586a8034bcce5cf7292d31f88b17cd11c9c70
RobertUnfried <8124@htl.rennweg.at>
2021-11-29 17:18:39 +0100

    Added Footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <Sidebar />
   <Login />
+  <Footer />
 </template>
 
 <script>
   import Login from './components/Login.vue'
+  import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Login,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer>
+        <p>Copyright &copy; 2021</p>
+        <a href="/about">About us</a>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style scoped>
+footer {
+    margin-top: 20px;
+    text-align: center;
+    background: #1a2815;
+    color: white;
+    position:absolute;
+    bottom:0;
+    width:100%;
+    height:100px;   
+}
+a {
+    color: white;
+}
+</style>

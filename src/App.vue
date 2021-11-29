da8c0c85500c01e11099d966a4f2e1f8683303e3
RobertUnfried <8124@htl.rennweg.at>
2021-11-29 12:41:10 +0100

    Added login component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <h1>HelloWorld</h1>
+  <Login />
 </template>
 
 <script>
+  import Login from './components/Login.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Login
+  }
 }
 </script>
 
---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Login</h1>
+  <form>
+    <div>
+      <label>User: </label>
+      <input type="text" name="username" placeholder="Username" />
+    </div>
+    <div>
+      <label>Password: </label>
+      <input type="password" name="password" placeholder="Password" />
+    </div>
+    <input type="submit" value="Login" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

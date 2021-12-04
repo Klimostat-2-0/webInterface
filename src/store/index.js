ec2fc76db0ec2c7578fb71f91acba031b93dd454
RobertUnfried <8124@htl.rennweg.at>
2021-12-04 14:58:26 +0100

    Added wrong password msg

--- src/store/index.js
+++ src/store/index.js
@@ -15,6 +15,9 @@ export default createStore({
       },
       server_offline(state) {
         state.showLoginError = "The login Server seems to be offline"
+      },
+      wrong_user(state) {
+        state.showLoginError = "You have entered a wrong username or password"
       }
     },
     actions: {
@@ -27,20 +30,18 @@ export default createStore({
               },
               body: user
             })
-            let data = await res.json()
-            print(res)
-            print(data)
+            if (res.status == 200){
+              let data = await res.json()
+              localStorage.setItem("tokens", data.tokens.access.token)
+              localStorage.setItem("username", data.user)
+              commit("auth_success", data.tokens.access.token, data.user)
+              router.push('Room')
+            }else {
+              commit("wrong_user")
+            }
           } catch(err) {
             commit("server_offline")
-            console.log("Handel the Error")
           }
-          /*console.log(data.tokens.access.token)
-          localStorage.setItem("tokens", data.tokens.access.token)
-          localStorage.setItem("username", data.user)
-          commit("auth_success", data.tokens.access.token, data.user)
-          if (res.status == 200){
-            router.push('Room')
-          }*/
         }
     },
     modules: {

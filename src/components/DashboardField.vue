7130bc97383b44af11db9615bf3650b02813f227
RobertUnfried <8124@htl.rennweg.at>
2021-12-05 15:27:16 +0100

    Added Dashboard - no live data yet

---
+++ src/components/DashboardField.vue
@@ -0,0 +1,52 @@
+<template>
+<div :style="{'background-color': calcColor}" class="dashboardComponent">
+    <router-link class="dashboardComponentLink" v-bind:id="stationID" to="/" tag="div">
+        <h2>{{stationID}}</h2>
+        <p>Current CO2 Level: {{ co2 }}</p>
+    </router-link>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+      co2Prop: {
+          type: String,
+      },stationIdProp: {
+          type: String,
+      },
+  },
+  computed: {
+    calcColor: function () {
+      if (this.co2 > 1500) return "#ff9187"
+      if (this.co2 > 800) return "#fcffb3"
+      return "#bbffb3"
+    }
+  },
+  data() {
+      return {
+          co2: String,
+          stationID: String
+      }
+  },
+  async created() {
+      this.co2 = this.co2Prop
+      this.stationID = this.stationIdProp
+}
+};
+</script>
+
+<style scoped>
+.dashboardComponent {
+    padding: 5%;
+    width: 18%;
+    border: 2px solid #2c3e50;
+    text-align: center;
+    margin: 20px;
+    background: chartreuse;
+}
+.dashboardComponentLink {
+    color: #1a2815;
+    text-decoration: none; 
+}
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,14 +1,23 @@
 <template>
   <h1>Dashboard</h1>
-  <p>Dashboard upcoming</p>
+  <p>This is an Overview of all Stations</p>
+  <div id="flexbox">
+    <dashboard-field :stationIdProp='"Room 375"' :co2Prop='"1564"'/>
+    <dashboard-field :stationIdProp='"Room 376"' :co2Prop='"453"'/>
+    <dashboard-field :stationIdProp='"Room 377"' :co2Prop='"855"'/>
+    <dashboard-field :stationIdProp='"Room 378"' :co2Prop='"855"'/>
+    <dashboard-field :stationIdProp='"Room 379"' :co2Prop='"855"'/>
+
+  </div>
 </template>
 
 <script>
-
+  import DashboardField from '../components/DashboardField'
   export default {
+
   name: 'Dashboard',
   components: {
-
+    DashboardField
   },
   methods: {
       
@@ -24,5 +33,9 @@
 </script>
 
 <style scoped>
-
+#flexbox {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
 </style>

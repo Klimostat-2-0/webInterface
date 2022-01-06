478a8aab0ffdfeea62e1de6440c7b9b3d5b659b3
RobertUnfried <8124@htl.rennweg.at>
2022-01-06 23:37:43 +0100

    Added better sidebar style

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,11 +3,27 @@
         <div class="element">
             <img src="../assets/logo.svg" alt="Logo">
         </div>
-        <router-link v-if="!this.$store.state.loggedIn" class="element linkHighlight" to="/" tag="div">Login</router-link>
-        <router-link v-if="this.$store.state.loggedIn" class="element linkHighlight" to="/Dashboard" tag="div">Dashboard</router-link>
-        <router-link v-if="this.$store.state.loggedIn" class="element linkHighlight" to="/adduser" tag="div">Add Account</router-link>
-        <router-link class="element linkHighlight" to="/about" tag="div">About</router-link>
-        <router-link @click="logOut" v-if="this.$store.state.loggedIn" class="element linkHighlight" to="/" tag="div">Logout</router-link>
+        
+        <router-link v-if="!this.$store.state.loggedIn" class="element linkHighlight" to="/" tag="div">
+        <i class="icon" :class="'fas fa-sign-in-alt'" />
+        Login
+        </router-link>
+        <router-link v-if="this.$store.state.loggedIn" class="element linkHighlight" to="/Dashboard" tag="div">
+        <i class="icon" :class="'fas fa-columns'" />
+        Dashboard
+        </router-link>
+        <router-link v-if="this.$store.state.loggedIn" class="element linkHighlight" to="/adduser" tag="div">
+        <i class="icon" :class="'fas fa-user-alt'" />
+        Add Account
+        </router-link>
+        <router-link class="element linkHighlight" to="/about" tag="div">
+        <i class="icon" :class="'fas fa-info'" />
+        About
+        </router-link>
+        <router-link @click="logOut" v-if="this.$store.state.loggedIn" class="element linkHighlight" to="/" tag="div">
+        <i class="icon" :class="'fas fa-sign-out-alt'" />
+        Logout
+        </router-link>
     </div>
 </template>
 
@@ -22,14 +38,18 @@ export default {
 </script>
 
 <style scoped>
+.icon {
+    display: inline-block;
+    width: 30px;
+}
 .sidebar {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align: left;
+  color: #1a2815;
   border-right: 2px solid #2c3e50;
-  background: white;
+  background: rgb(255, 255, 255);
 
   float: left;
   position: fixed;
@@ -43,13 +63,15 @@ export default {
 }
 .element {
     padding: 5%;
+    margin: 4px;
+    border-radius: 10px;
     width:auto;
-    border-bottom: 2px solid #2c3e50;
+
     color: #1a2815;
     text-decoration: none;
 }
 
 .linkHighlight:hover {
-    background: #cecece;
+    background: rgba(20, 20, 20, 0.3);
 }
 </style>

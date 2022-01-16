fdaa1f38f81033dedda0de77e2058ebeac0392e2
RobertUnfried <8124@htl.rennweg.at>
2022-01-16 11:21:26 +0100

    added responsive user form

--- src/pages/AddAccount.vue
+++ src/pages/AddAccount.vue
@@ -1,30 +1,35 @@
 <template>
-  <h1>Add User</h1>
-  <form @submit.prevent="onClick">
-    <div class="form-floating mb-3">
-      <input class="form-control" v-model="username" type="text" name="username" placeholder="Username" required/>
-      <label class="form-floating mb-4">Username: </label>
-    </div>
-    <div class="form-floating mb-3">
-      <input class="form-control" v-model="email" type="email" name="email" placeholder="Email" required/>
-      <label class="form-floating mb-4">Email: </label>
-    </div>
-    <div class="form-floating mb-3">
-      <input class="form-control" v-model="password" type="password" name="password" placeholder="Password" required/>
-      <label class="form-floating mb-4">Passwort: </label>
-    </div>
-    <div class="form-floating mb-3">
-      <input class="form-control" v-model="secondPassword" type="password" name="password" placeholder="Password" required/>
-      <label class="form-floating mb-4">Repeat password: </label>
-    </div>
-    <p :style="{ color: msgColor}" class="userMsg" v-if="successMsg != null">{{successMsg}}</p>
-    <input class="custom-btn w-25" type="submit" value="Create" />
-  </form>
-  <hr>
-  <h1>All Users</h1>
-  <div id="flexbox" v-if="!isFetching">
-    <user @delete-user="deleteUser" :key="user.id" v-for="user in users" :username="user.name" :email="user.email" :role="user.role" :userId="user.id"/>
+<div id="user-edit-wrapper">
+  <div class="shadow-box">
+    <h1>Add User</h1>
+    <form id="add-user-form" @submit.prevent="onClick">
+      <div class="form-floating mb-3">
+        <input class="form-control" v-model="username" type="text" name="username" placeholder="Username" required/>
+        <label class="form-floating mb-4">Username: </label>
+      </div>
+      <div class="form-floating mb-3">
+        <input class="form-control" v-model="email" type="email" name="email" placeholder="Email" required/>
+        <label class="form-floating mb-4">Email: </label>
+      </div>
+      <div class="form-floating mb-3">
+        <input class="form-control" v-model="password" type="password" name="password" placeholder="Password" required/>
+        <label class="form-floating mb-4">Passwort: </label>
+      </div>
+      <div class="form-floating mb-3">
+        <input class="form-control" v-model="secondPassword" type="password" name="password" placeholder="Password" required/>
+        <label class="form-floating mb-4">Repeat password: </label>
+      </div>
+      <p :style="{ color: msgColor}" class="userMsg" v-if="successMsg != null">{{successMsg}}</p>
+      <input class="custom-btn" type="submit" value="Create" />
+    </form>
   </div>
+  <div class="shadow-box">
+  <h1>All Users</h1>
+    <div id="flexbox" v-if="!isFetching">
+      <user @delete-user="deleteUser" :key="user.id" v-for="user in users" :username="user.name" :email="user.email" :role="user.role" :userId="user.id"/>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -108,35 +113,5 @@ import User from '../components/User'
 </script>
 
 <style scoped>
-h1{
-    margin-bottom: 50px;
-}
-div {
-    margin-top: 10px;
-    margin-bottom: 10px;
-}
-.createButton {
-  background-color: #1a2815;
-  border: none;
-  color: #dbff78;
-  padding: 5px 20px;
-  display: inline-block;
-  font-size: 16px;
-  margin-top: 30px;
-}
-.error {
-  font-size: 14px;
-  color: red;
-}
-.formElement {
-  display: inline-block;
-  width: 200px;
-  text-align: left;
-  margin: 0%;
-}
-#flexbox {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+@import "../styles/userEdit.css";
 </style>
